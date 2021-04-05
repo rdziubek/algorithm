b9befca8a0066621a2b878632e7c2b7cece410c5
witampanstwa <robert.dziubek5@gmail.com>
2021-04-05 22:22:36 +0200

    Subrarray fixup

--- max_subarray_brute.py
+++ max_subarray_brute.py
@@ -1,15 +1,21 @@
 def force_find(array):
     biggest = array[0]
+    biggest_start = 0
+    biggest_end = 0
 
     for i in range(len(array)):
-        current = array[i]
+        current = 0
 
-        for j in range(len(array) - i):
+        for j in range(i, len(array)):
             current += array[j] 
         
-        biggest = current if current > biggest else biggest
+            if current > biggest: 
+                biggest = current
+                biggest_start = i
+                biggest_end = j
     
+    print(biggest_start, biggest_end)
     return biggest
 
 
-print(force_find([0, 1, 0, 1, 2, 3]))
+print(force_find([0, 1, -10, 1, 2, 3]))
---
+++ max_subarray_kadane.py
@@ -0,0 +1,14 @@
+def kadane(A):
+    max_so_far = 0
+    max_ending_here = 0
+    
+    for i in A:
+        max_ending_here = max_ending_here + i
+        max_ending_here = max(max_ending_here, 0)
+        max_so_far = max(max_so_far, max_ending_here)
+    
+    return max_so_far
+
+
+print(kadane([0, 1, -10, 1, 2, 3]))
+
