68794212d655290962ceee4a484b1f7a60d910dd
witampanstwa <robert.dziubek5@gmail.com>
2021-04-09 17:31:49 +0200

    Bubble, selection

---
+++ selection_sort.py
@@ -0,0 +1,14 @@
+def sort(array):
+    for i in range(len(array) - 1):
+        min_index = i
+
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_index]:
+                min_index = j
+
+        array[i], array[min_index] = array[min_index], array[i]
+
+    return array
+
+
+print(sort([3, 2, 11, -1, 0]))
