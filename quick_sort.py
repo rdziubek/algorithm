9241f6b2e787d1e4db3d233f3ca4bf2a520fb5f2
witampanstwa <robert.dziubek5@gmail.com>
2021-04-11 19:49:02 +0200

    Quick sort

---
+++ quick_sort.py
@@ -0,0 +1,33 @@
+"""
+NOTE:
+    `left < j` and `right > i` checks make sure there is something to actually sort
+"""
+
+
+def sort(array, left, right):
+    i = left
+    j = right
+
+    pivot = array[(left + right) // 2]
+
+    while i <= j:
+        while array[i] < pivot:
+            i += 1
+        while array[j] > pivot:
+            j -= 1
+
+        if i <= j:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
+
+    if left < j:
+        sort(array, left, j)
+    if right > i:
+        sort(array, i, right)
+
+    return array
+
+
+array = [3, 2, 11, -1, 0]
+print(sort(array, 0, len(array) - 1))
