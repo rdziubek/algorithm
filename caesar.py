4636dde0f3be56e231e76aa91699f81585587b48
witampanstwa <robert.dziubek5@gmail.com>
2021-04-13 08:54:18 +0200

    Update Ceasar's

--- caesar.py
+++ caesar.py
@@ -1,17 +1,29 @@
-def cipher(key: int, content: list):
-    ciphered = []
+def cipher(key: int, content):
+    result = []
 
-    for ch in content:
-        candidate: int = ord(ch) + key
+    for i in range(len(content)):
+        candidate: int = ord(content[i]) + key
 
         if candidate > ord('z'):
             candidate -= 26
         elif candidate < ord('a'):
             candidate += 26
 
-        ciphered.append(chr(candidate))
+        result.append(chr(candidate))
 
-    return ciphered
+    return result
 
 
-print(cipher(2, list('xyz')))
+def cipher_custom(key: int, alphabet, content):
+    result = []
+
+    for i in range(len(content)):
+        for j in range(len(alphabet)):
+            if content[i] == alphabet[j]:
+                result += [alphabet[(j + key) % len(alphabet)]]
+
+    return result
+
+
+print(cipher(2, 'xyz'))
+print(cipher_custom(2, 'abcdefghijklmnopqrstuvwxyz', 'xyz'))
