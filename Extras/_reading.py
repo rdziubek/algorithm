dd3db1a616ad048c3dd8ceb2629a018ae73bec19
witampanstwa <robert.dziubek5@gmail.com>
2021-04-13 20:56:24 +0200

    GitHub Pages

--- Extras/_reading.py
+++ Extras/_reading.py
@@ -1,4 +1,4 @@
-with open('../_in/galerie_przyklad.txt') as file:
+with open('../in/galerie_przyklad.txt') as file:
     rows = [row.strip().split() for row in file.readlines() if row.strip()]
 
 for row in rows:
