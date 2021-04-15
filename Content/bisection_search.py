70e89b007f658fc01115843cfdd16969332e21fa
witampanstwa <robert.dziubek5@gmail.com>
2021-04-15 12:01:09 +0200

    Add bisection array search

---
+++ Content/bisection_search.py
@@ -0,0 +1,24 @@
+"""
+
+O(log n)
+"""
+
+
+def search(array, target):
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+
+        if array[middle] < target:
+            left = middle + 1
+        elif array[middle] > target:
+            right = middle - 1
+        else:
+            return middle
+
+    return -1
+
+
+print(search([-1, 0, 2, 3, 11], 3))
