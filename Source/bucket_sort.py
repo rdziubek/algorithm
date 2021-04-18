f9db58acf1289f26546ab5265c5c1ff77f06da0b
witampanstwa <robert.dziubek5@gmail.com>
2021-04-18 18:09:10 +0200

    Update bucket sort

--- Source/bucket_sort.py
+++ Source/bucket_sort.py
@@ -6,8 +6,14 @@
 5. Print buckets.
 
 NOTE:
-    `array[i] / size` is not floored (by integer division, as opposed to
-    the used integer cast) so as not to hinder negative value handling
+    `array[i] / size` is not floored (by floor division, as opposed to
+    the used integer cast from a real division) so as not to
+    encounter rounding errors caused by truncating instead of
+    rounding float numbers down.
+
+    `j` figures actual bucket's index out.
+
+    Elements 'overflowing' the bucket size are put into the last container.
 
 O(n²)
 """
@@ -16,27 +22,27 @@ import insertion_sort
 
 
 def sort(array, max_value):
-    size = max_value / len(array)
+    span = max_value / len(array)
 
     buckets = []
     for _ in range(len(array)):
         buckets += [[]]
 
     for i in range(len(array)):
-        j = int(array[i] / size)
+        j = int(array[i] / span)
         if j != len(array):
             buckets[j] += [array[i]]
         else:
-            buckets[len(array) - 1] += [array[i]]
+            buckets[j - 1] += [array[i]]
 
     for i in range(len(array)):
         insertion_sort.sort(buckets[i])
 
     result = []
     for i in range(len(array)):
-        result = result + buckets[i]
+        result += buckets[i]
     return result
 
 
-array = [3, 2, 11, -1, 0]
+array = [0.48, 0.27, 0.12, 0.21, 0.43, 0.25]
 print(sort(array, max(array)))
