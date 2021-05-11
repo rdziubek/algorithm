395bc5d0e96918cbb6a453b9bb5f95e276d4991f
witampanstwa <robert.dziubek5@gmail.com>
2021-05-11 14:58:50 +0200

    Unify fractional optimisations

--- Source/perfect_number.py
+++ Source/perfect_number.py
@@ -3,6 +3,9 @@ NOTE:
     Indexing has to start from the second index, so as not to divide number by 1 when adding two divisors.
     While instead of a for loop is used as for loop cant be constructed without using math.sqrt
 
+    When checking up to the inclusive square of the number, it has to be verified that one divisor
+    is not being added 2 times at once.
+
 O(√n)
 """
 
@@ -14,9 +17,11 @@ def is_perfect(number):
         return False
 
     i = 2
-    while i * i < number:
+    while i * i <= number:
         if number % i == 0:
-            total += i + number // i
+            total += i
+            if number // i != i:
+                total += number // i
         i += 1
 
     return total == number
