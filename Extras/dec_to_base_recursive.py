1e53e93292d85f7f6a54c8c1211daac49fdcb64e
witampanstwa <robert.dziubek5@gmail.com>
2021-05-16 09:21:25 +0200

    Unify recursive decimal to base implementation

--- Extras/dec_to_base_recursive.py
+++ Extras/dec_to_base_recursive.py
@@ -1,7 +1,10 @@
 def change_base(number, base):
+    _returned = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                 'A', 'B', 'C', 'D', 'E', 'F']
+
     if number > 0:
         change_base(number // base, base)
-        print(number % base)
+        print(_returned[number % base])
 
 
 """EVALUATION"""
